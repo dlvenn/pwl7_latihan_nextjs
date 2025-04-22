5abbbde4776546379ad2f63ad8a1d2cfd7921e17
dlvenn <desyanalavenia6@gmail.com>
2025-04-22 13:41:26 +0700

    first commit

---
+++ src/app/api/matkul/route.js
@@ -0,0 +1,9 @@
+import prisma from "@/lib/prisma";
+
+export async function GET () {
+    const data = await prisma.matkul.findMany({
+        orderBy: { id: 'asc' },
+    });
+
+    return new Response(JSON.stringify(data), { status: 200 });
+}
